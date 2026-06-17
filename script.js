b9a7d841c14d016446d97bcc04581e69e79c9571
jeanbaptistenoureau-hub <jeanbaptiste.noureau@gmail.com>
2026-06-17 12:48:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function connexion() {
+function connexion(){
 
-const email = document.getElementById("email").value.trim();
-const mdp = document.getElementById("mdp").value.trim();
+let email=document.getElementById("email").value;
+let mdp=document.getElementById("mdp").value;
 
-if (email === "directeur@ndm.fr" && mdp === "directeur123") {
+if(email==="directeur@ndm.fr" && mdp==="1234"){
 
-document.getElementById("login").style.display =
+document.getElementById("login").style.display="none";
+document.getElementById("menu").style.display="block";
+
+document.getElementById("bonjour").innerText="Bienvenue Directeur";
+
+}else{
+
+alert("Identifiants incorrects");
+
+}
+
+}
+
+function ouvrir(page){
+
+document.getElementById("menu").style.display="none";
+
+document.querySelectorAll(".page").forEach(p=>p.style.display="none");
+
+document.getElementById(page).style.display="block";
+
+}
+
+function retour(){
+
+document.querySelectorAll(".page").forEach(p=>p.style.display="none");
+
+document.getElementById("menu").style.display="block";
+
+}
