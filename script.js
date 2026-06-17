4916876559c2cfd24be5d37052805692fe778012
jeanbaptistenoureau-hub <jeanbaptiste.noureau@gmail.com>
2026-06-17 12:41:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-function connexion(){
+function connexion() {
 
-let ecole=document.getElementById("ecole").value;
-let identifiant=document.getElementById("identifiant").value;
-let mdp=document.getElementById("mdp").value;
+const email = document.getElementById("email").value.trim();
+const mdp = document.getElementById("mdp").value.trim();
 
-if(
-ecole==="NDM-DEMO" &&
-identifiant==="directeur" &&
-mdp==="directeur123"
-){
+if (email === "directeur@ndm.fr" && mdp === "directeur123") {
 
-document.getElementById("login").style.display="none";
-document.getElementById("menu").style.display="block";
-
-document.getElementById("bonjour").innerHTML=
-"Bienvenue Directeur";
-
-}else{
-
-alert("Code école ou mot de passe incorrect");
-
-}
-
-}
-
-function ouvrir(page){
-
-document.getElementById("menu").style.display="none";
-
-document.querySelectorAll(".page").forEach(
-p=>p.style.display="none"
-);
-
-document.getElementById(page).style.display="block";
-
-}
-
-function retour(){
-
-document.querySelectorAll(".page").forEach(
-p=>p.style.display="none"
-);
-
-document.getElementById("menu").style.display="block";
-
-}
+document.getElementById("login").style.display =
